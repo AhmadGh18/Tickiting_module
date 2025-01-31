bbcfad1e8d517b893ef986139a9f617494d54723
malekjoe <malekjokhadar17@gmail.com>
2025-01-31 13:46:36 +0200

    center TicketingForm component and add Header to the Ticketing page

--- src/app/[locale]/Ticketing/page.tsx
+++ src/app/[locale]/Ticketing/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import Body from './Components/Body'
+import Header from './Components/Header'
 
 const page = () => {
   return (
-    <div className='h-auto '>
-      
+    <div>
+        <Header />
         <Body/>
     
     </div>
